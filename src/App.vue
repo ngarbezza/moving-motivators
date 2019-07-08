cacfb33f19ffd4770ec15da818ac8af8c93c21d2
Nahuel Garbezza <n.garbezza@gmail.com>
2019-07-08 19:20:04 -0300

    render motivator names and add ability to sort them

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,15 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Motivators/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import Motivators from './components/Motivators.vue';
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld,
-  },
+  components: { Motivators },
 };
 </script>
 
---
+++ src/components/Motivators.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="motivators">
+    <h1>Moving Motivators</h1>
+
+    <draggable v-model="motivators" group="people" @start="drag=true" @end="drag=false">
+      <div class="motivator" v-for="motivator in motivators" :key="motivator.id">
+        {{motivator.name}}
+      </div>
+    </draggable>
+  </div>
+</template>
+
+<script>
+import draggable from 'vuedraggable';
+
+const motivators = [
+  { name: 'Aceptación' },
+  { name: 'Curiosidad' },
+  { name: 'Libertad' },
+  { name: 'Estatus' },
+  { name: 'Meta' },
+  { name: 'Honra' },
+  { name: 'Maestría' },
+  { name: 'Orden' },
+  { name: 'Poder' },
+  { name: 'Relaciones' },
+];
+
+export default {
+  name: 'Motivators',
+  props: { title: String },
+  components: { draggable },
+  data: () => ({ motivators }),
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+
+a {
+  color: #42b983;
+}
+
+.motivators {
+}
+
+.motivator {
+  display: inline-block;
+  margin: 0 2em;
+}
+</style>
