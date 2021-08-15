8c0dc5c57d7c3484ddb5bd2f1dd56059ca1d9203
Nahuel Garbezza <n.garbezza@gmail.com>
2021-08-15 16:55:20 -0300

    upgrade to Vue 3 and improve styles

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,21 @@
 <template>
-  <div id="app">
-    <Motivators/>
-  </div>
+  <Motivators/>
 </template>
 
 <script>
 import Motivators from './components/Motivators.vue';
 
 export default {
-  name: 'app',
+  name: 'App',
   components: { Motivators },
 };
 </script>
 
 <style>
-#app {
+* {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
+  color: #24262AFF;
 }
 </style>
