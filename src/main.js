563d9148aeb8e68f26eac5a67d5a2f707709ee23
Skilb <sebastian.kilb1@gmail.com>
2021-10-09 15:23:47 +0200

    fixed eslint errors

--- src/main.js
+++ src/main.js
@@ -7,7 +7,9 @@ import es from './translate/es.json';
 import it from './translate/it.json';
 import de from './translate/de.json';
 
-const messages = { en, es, it, de };
+const messages = {
+  en, es, it, de,
+};
 const locale = 'en';
 const i18n = createI18n({ locale, messages });
 
