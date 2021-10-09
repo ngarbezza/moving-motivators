c6660dcc87b9aed573c39ba0c2c703a5ea806956
Skilb <sebastian.kilb1@gmail.com>
2021-10-09 15:08:18 +0200

    added german translation

--- src/main.js
+++ src/main.js
@@ -5,8 +5,9 @@ import App from './App.vue';
 import en from './translate/en.json';
 import es from './translate/es.json';
 import it from './translate/it.json';
+import de from './translate/de.json';
 
-const messages = { en, es, it };
+const messages = { en, es, it, de };
 const locale = 'en';
 const i18n = createI18n({ locale, messages });
 
