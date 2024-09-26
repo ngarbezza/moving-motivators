f89d1e8470e024cdc8726957090e2e08e19e8f33
Nahuel Garbezza <n.garbezza@gmail.com>
2024-09-26 06:38:09 -0300

    lint: fix accessibility lint rule

--- src/components/LocaleSelector.vue
+++ src/components/LocaleSelector.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="locale-changer">
     <span>Change language: </span>
-    <label for="locale-select" />
-    <select v-model="$i18n.locale" name="locale-select">
-      <option v-for="locale in $i18n.availableLocales" :key="`locale-${locale}`" :value="locale">
-        {{ locale }}
-      </option>
-    </select>
+    <label>
+      <select v-model="$i18n.locale" name="locale-select">
+        <option v-for="locale in $i18n.availableLocales" :key="`locale-${locale}`" :value="locale">
+          {{ locale }}
+        </option>
+      </select>
+    </label>
   </div>
 </template>
 
